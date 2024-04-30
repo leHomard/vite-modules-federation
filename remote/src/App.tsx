c4f12fc4a726241c37084bb82647458df4aac82f
Omar Harras <omar.harras.externe@foncia.com>
2024-04-30 11:01:43 +0200

    chore: init remote

---
+++ remote/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Title } from "./Title";
+
+function App() {
+  return (
+    <>
+      <Title />
+    </>
+  );
+}
+
+export default App;
---
+++ remote/src/Title.tsx
@@ -0,0 +1,6 @@
+
+export const Title = () => {
+  return (
+    <div>Remote title</div>
+  )
+}
